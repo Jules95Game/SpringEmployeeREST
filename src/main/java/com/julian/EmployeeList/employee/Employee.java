f8ca1f06758b9e7266022d15111e782ffa4fcc99
Jules95Game <romp.julian1@gmail.com>
2024-04-09 15:52:51 +0200

    added Employee model

---
+++ src/main/java/com/julian/EmployeeList/employee/Employee.java
@@ -0,0 +1,23 @@
+package com.julian.EmployeeList.employee;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+public class Employee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String name;
+    private int age;
+}
