e702d856f459441faa8b6576bb3abab2b5f03643
Jules95Game <romp.julian1@gmail.com>
2024-04-09 14:21:55 +0200

    Initial commit

---
+++ src/test/java/com/julian/EmployeeList/EmployeeListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julian.EmployeeList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
