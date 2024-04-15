814005d17f75a8322bba6bf4568546aef04fc844
Jules95Game <romp.julian1@gmail.com>
2024-04-15 12:16:47 +0200

    Cleanup

--- src/main/java/com/julian/EmployeeList/EmployeeListApplication.java
+++ src/main/java/com/julian/EmployeeList/EmployeeListApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EmployeeListApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmployeeListApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeListApplication.class, args);
+    }
 
 }
