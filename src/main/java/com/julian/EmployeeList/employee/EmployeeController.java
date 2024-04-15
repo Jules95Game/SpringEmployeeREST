93b1b4b3da94e3bed3af3ddc7bb73de2ee949473
Jules95Game <romp.julian1@gmail.com>
2024-04-15 13:57:54 +0200

    Added controller logic and findAll method

--- src/main/java/com/julian/EmployeeList/employee/EmployeeController.java
+++ src/main/java/com/julian/EmployeeList/employee/EmployeeController.java
@@ -1,4 +1,21 @@
 package com.julian.EmployeeList.employee;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/employees")
+@RequiredArgsConstructor
 public class EmployeeController {
+
+    private final EmployeeRepository employeeRepository;
+
+    @GetMapping
+    public List<Employee> findAll() {
+        return employeeRepository.findAll();
+    }
 }
