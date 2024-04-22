4b81037fe73d81985d67b36aab907499b92ff2e6
Jules95Game <romp.julian1@gmail.com>
2024-04-22 15:57:33 +0200

    Added getById and createEmployee methods

--- src/main/java/com/julian/EmployeeList/employee/EmployeeController.java
+++ src/main/java/com/julian/EmployeeList/employee/EmployeeController.java
@@ -1,11 +1,15 @@
 package com.julian.EmployeeList.employee;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/employees")
@@ -17,5 +21,22 @@ public class EmployeeController {
     @GetMapping
     public List<Employee> findAll() {
         return employeeRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getById(@PathVariable UUID id) {
+        Optional<Employee> employee = employeeRepository.findById(id);
+        if (employee.isPresent()) {
+            return ResponseEntity.ok(employee.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee newEmployee, UriComponentsBuilder ucb) {
+        Employee savedEmployee = employeeRepository.save(newEmployee);
+        URI location = ucb.path("employees/{id}").buildAndExpand(savedEmployee.getId()).toUri();
+        return ResponseEntity.created(location).body(savedEmployee);
     }
 }
