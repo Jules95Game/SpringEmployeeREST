15f725ab03e9fab7a1deeafa4ae8abe879ae6650
Jules95Game <romp.julian1@gmail.com>
2024-05-06 12:43:50 +0200

    Added deleteEmployee method

--- src/main/java/com/julian/EmployeeList/employee/EmployeeController.java
+++ src/main/java/com/julian/EmployeeList/employee/EmployeeController.java
@@ -34,9 +34,21 @@ public class EmployeeController {
     }
 
     @PostMapping
-    public ResponseEntity<Employee> createEmployee(@RequestBody Employee newEmployee, UriComponentsBuilder ucb) {
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee newEmployee,
+                                                   UriComponentsBuilder ucb) {
         Employee savedEmployee = employeeRepository.save(newEmployee);
         URI location = ucb.path("employees/{id}").buildAndExpand(savedEmployee.getId()).toUri();
         return ResponseEntity.created(location).body(savedEmployee);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Employee> deleteEmployee(@PathVariable UUID id) {
+        Optional<Employee> oldEmployee = employeeRepository.findById(id);
+        if (oldEmployee.isPresent()) {
+            employeeRepository.delete(oldEmployee.get());
+            return ResponseEntity.ok(oldEmployee.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
