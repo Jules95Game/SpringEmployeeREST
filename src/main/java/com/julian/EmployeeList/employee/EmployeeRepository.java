e46dd466b3f54c004c596dc009b98adf69b728c8
Jules95Game <romp.julian1@gmail.com>
2024-05-24 11:33:11 +0200

    Added JPA query methods

--- src/main/java/com/julian/EmployeeList/employee/EmployeeRepository.java
+++ src/main/java/com/julian/EmployeeList/employee/EmployeeRepository.java
@@ -2,7 +2,11 @@ package com.julian.EmployeeList.employee;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface EmployeeRepository extends JpaRepository<Employee, UUID> {
+    List<Employee> findByNameContainsIgnoreCase(String query);
+
+    List<Employee> findByNameContainsIgnoreCaseAndAgeLessThanEqual(String query, int age);
 }
